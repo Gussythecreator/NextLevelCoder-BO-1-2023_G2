b3e648efda1300ac4dc9beb4b3f1e7afd5cb3348
Gustavo Ernesto Villarroel Espinoza <Gustavo.villarroel@fundacion-jala.org>
2023-03-02 19:28:25 -0400

    Update dinosaur.py

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,13 @@
+class dinosaur:
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+#Crear clase Dinosaur, metodo constructor, metodo update, draw pass (entregar)
+# Investigar que son los sprites
+# Investigar herencia en programacion 
