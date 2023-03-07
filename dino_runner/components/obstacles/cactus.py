3b5e36f6760bd13e71749adc5d0e84d9d34be8e7
Gustavo Ernesto Villarroel Espinoza <Gustavo.villarroel@fundacion-jala.org>
2023-03-07 16:28:06 -0400

    Obstaculos

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -6,4 +6,4 @@ class Cactus(Obstacle):
     def __init__(self, image_list):
         self.type = random.randint(0, 2)
         super().__init__(image_list, self.type) #llama al metodo constructor padre y setea todo
-        self.rect.y = 310
+        self.rect.y = 325
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,6 +1,8 @@
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.bird import Bird
+from dino_runner.utils.constants import SMALL_CACTUS, BIRD, LARGE_CACTUS
 import pygame
+import random
 
 class ObstacleManager:
     
@@ -8,8 +10,21 @@ class ObstacleManager:
         self.obstacles = []
 
     def update(self, game):
+
+    
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            if random.randint(0, 2) == 0:
+                self.obstacles.append(Cactus(SMALL_CACTUS))
+            elif random.randint(0, 1) == 1:
+                self.obstacles.append(Bird(BIRD))
+            elif random.randint(0, 2) == 2:
+                self.obstacles.append(Cactus(LARGE_CACTUS))
+
+           # self.obstacles.append(Cactus(SMALL_CACTUS))
+           # self.obstacles.append(Cactus(LARGE_CACTUS))
+           # self.obstacles.append(Bird(BIRD))
+
+
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
